dcbb9c36df7f19c761b075e80b1f46c131a911c8
ChebaCheba <bastiangaxiola@gmail.com>
2020-02-05 21:00:50 -0600

    Buffer progress bar, other GUI fixes

--- src/Buffer.java
+++ src/Buffer.java
@@ -1,6 +1,7 @@
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.ArrayList;
+import javax.swing.JProgressBar;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JTextField;
@@ -13,8 +14,10 @@ public class Buffer {
     private static JTable tablitaP, tablitaC;
     private static DefaultTableModel modelP, modelC;
     private static JTextField txtCompleted;
+    private static JProgressBar progressB;
+    private static int percentage;
     
-    Buffer(JTable tablitaP, JTable tablitaC, JTextField textito) {
+    Buffer(JTable tablitaP, JTable tablitaC, JTextField textito, JProgressBar progressB) {
         this.buffer = new ArrayList<String>();
         this.id = 0;
         this.tablitaP = tablitaP;
@@ -22,7 +25,9 @@ public class Buffer {
         this.modelC = (DefaultTableModel) tablitaC.getModel();
         this.modelP = (DefaultTableModel) tablitaP.getModel();
         this.txtCompleted = textito;
+        this.progressB = progressB;
         // Defaults
+        this.percentage = 0;
         this.length = 1;
     }
     
@@ -53,10 +58,13 @@ public class Buffer {
                 Logger.getLogger(Buffer.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        this.id++;
-        this.buffer.add(this.id+"$"+product);
-        Buffer.insertRowP(this.id+"",idP,product);
-        notify();
+        if(this.buffer.size() < this.length){
+            this.id++;
+            this.buffer.add(this.id+"$"+product);
+            Buffer.insertRowP(this.id+"",idP,product);
+            this.progressB.setValue(this.getPercentage());
+            notify();
+        }
     }
     
     static int count = 1;
@@ -78,8 +86,12 @@ public class Buffer {
     }
     
     synchronized static void delRowP(int index){
-        Buffer.modelP.removeRow(index);
-        Buffer.tablitaP.setModel(modelP);
+        try{
+            Buffer.modelP.removeRow(index);
+            Buffer.tablitaP.setModel(modelP);
+        } catch(ArrayIndexOutOfBoundsException error){
+            
+        }
     }
     
     public void clearModelC(){
@@ -96,7 +108,11 @@ public class Buffer {
         this.length = length;
     }
     
-    public int getSize(){
-        return this.buffer.size();
+    public int getPercentage(){
+        return (int) (this.buffer.size()*100/this.length);
     }
+    
+    /*public int getSize(){
+        return this.buffer.size();
+    }*/
 }
